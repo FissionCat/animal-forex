be8eeab14f4d0c8992ecce74cea737399f9c9d19
Maria <thunderstarry@gmail.com>
2014-08-30 23:19:36 +0100

    Add angular goodness

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var animalforexApp = angular.module('animalforexApp', []);
+(function() {
+    var animalforexApp = angular.module('animalforexApp', []);
+
+    animalforexApp.controller('ItemTableCtrl', ['$scope', function($scope) {
+        $scope.items = [{
+            id: 1,
+            name: 'amber'
+        }, {
+            id: 2,
+            name: 'ammonite'
+        }, {
+            id: 3,
+            name: 'modern wood chair'
+        }];
+    }]);
+})();
