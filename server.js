ed207404dd67a012752f239c5b1e16c62addc929
Maria <thunderstarry@gmail.com>
2014-08-31 18:13:40 +0100

    Fix page refresh breaking app
    User is redirected to home page instead.

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Re-route page requests to fix refreshing page giving a 404
+app.get('/list', function(req, res, next) {
+	res.redirect('/');
+});
+
+app.get('/offers', function(req, res, next) {
+	res.redirect('/');
+});
+
 app.listen(8080);
 console.log('Server listening on port 8080');
