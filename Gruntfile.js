320eb84b564708ec51476eb04349d2746924f17d
Maria Chapman <thunderstarry@gmail.com>
2015-02-09 12:57:19 +0000

    Change tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,25 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'static/css/all.css': 'static/scss/all.scss'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
-	});
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'static/css/all.css': 'static/scss/all.scss'
+                }
+            }
+        },
+        watch: {
+            scss: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            },
+            node: {
+                files: 'server/*.js'
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
 };
